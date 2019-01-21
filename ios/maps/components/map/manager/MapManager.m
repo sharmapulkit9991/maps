71eb08deb7acc135e437790b0aa01b373d97bdde
Pulkit <pulkit.sharma@wadi.com>
2019-01-21 12:48:21 +0530

    add location permission

--- ios/maps/components/map/manager/MapManager.m
+++ ios/maps/components/map/manager/MapManager.m
@@ -8,15 +8,47 @@
 
 #import "MapManager.h"
 #import "MapView.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface MapManager()<CLLocationManagerDelegate> {
+  CLLocationManager *locationManager;
+  MapView *mapView;
+}
+@end
 
 @implementation MapManager
 
 RCT_EXPORT_MODULE()
 
-- (UIView *)view
-{
-
-  return [[MKMapView alloc] init];
+- (UIView *)view {
+  [self getCurrentLocation];
+  mapView = [[MapView alloc] init];
+  [mapView setShowsUserLocation:YES];
+  return mapView;
 }
+
+- (void)getCurrentLocation {
+  locationManager = [[CLLocationManager alloc] init];
+  locationManager.delegate = self;
+  locationManager.distanceFilter = kCLDistanceFilterNone;
+  locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  
+  if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+    [locationManager requestWhenInUseAuthorization];
+  
+  [locationManager startUpdatingLocation];
+}
+
+#pragma mark CLLocationManagerDelegate methods
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray<CLLocation *> *)locations API_AVAILABLE(ios(6.0), macos(10.9)) {
+  
+  CLLocation *latestLocation = [locations lastObject];
+  NSLog(@"Updated Location is %f, %f", latestLocation.coordinate.latitude, latestLocation.coordinate.longitude);
+  [mapView setCenterCoordinate:latestLocation.coordinate];
+  
+}
+
 
 @end
