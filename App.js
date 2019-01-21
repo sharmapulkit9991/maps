68cb887684730e7de0594e063bedeb0a9ab4e31d
Pulkit <pulkit.sharma@wadi.com>
2019-01-21 12:21:57 +0530

    add boilerplate UI

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-import MapView from './src/mapView';
+import Maps from './src';
 
 
 
@@ -17,19 +8,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <MapView style={styles.container}>
-
-      </MapView>
+      <Maps/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
-});
---
+++ src/index.js
@@ -0,0 +1,66 @@
+
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet, Text, View, TextInput, Dimensions} from 'react-native';
+
+import MapView from './mapView';
+
+
+
+type Props = {
+
+  isSearchable? : boolean,
+  header? : boolean,
+  heading? : string
+};
+export default class Maps extends Component<Props> {
+
+  static defaultProps = {
+    isSearchable: true,
+    header: true,
+    heading: 'Enter Location'
+  }
+
+  render() {
+
+  const { isSearchable, heading, header } = this.props;
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        {header && <View style = {styles.header}>
+          <Text>
+            {heading}
+          </Text>
+          {
+            isSearchable && <TextInput style={styles.searchField}>
+            </TextInput>
+          }
+        </View>
+       }
+      <MapView style={styles.container}>
+
+      </MapView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+  },
+  searchField: {
+    height: 50,
+    width: Dimensions.get('window').width - 30,
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: 1,
+    marginHorizontal: 10
+  }
+
+});
